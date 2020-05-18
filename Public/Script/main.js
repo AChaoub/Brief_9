17e79e35571d77e1d57715758b1072a0199d814d
Mehdi-Choukri <medelmehdi.choukri@gmail.com>
2020-05-18 05:02:35 +0000

    panier fonctionnel

--- Public/Script/main.js
+++ Public/Script/main.js
@@ -1,146 +1,69 @@
-// var BTNplus = document.getElementsByClassName('produit_commande_cible--qte+');
-// var QteSpanValue= document.getElementsByClassName('id_span_qte');
-
-// var BTNminus = document.getElementsByClassName('class_btn_minus');
-
-calcul_price();
 $(function () {
-
+        
+       
+    calcul_price();
     //diminue quantité
+ 
+        $(".produit_commande_cible--qte-NEW").on('click', function (e) {
+          
+            var number = Number($(this).next('.produit_commande_cible--qteNumber').text());
+            if(number>0)
+            {
+                number--;
+                var prix = Number($(this).prev().prev('.produit_commande_cible--prixFixe').text());
+                var expression =prix*number.toFixed(2);
+                $(this).prev('.produit_commande_cible--prix').text((expression).toFixed(2));
+                $(this).next('.produit_commande_cible--qteNumber').text(number);
+                $(this).next('.produit_commande_cible--qteNumber').text(number);
+                $(this).html("<button class='id_btn_plus'>-</button>");
+                $(this).next().next('.PDQPLUSNEW').html("<button class='id_btn_plus'>+</button>");
+            }
+            else
+            {
+                  number++;
+                $(this).html("<button  class='class_btn_minus'><i style='color:red;' class='fas fa-trash-alt'></i></button>");
+                $(this).next('.produit_commande_cible--qteNumber').text("0");
+            }
+            remplir_panier();
+            calcul_price();
+        
+     
+    
+        });
 
-    $(".produit_commande_cible--qte-").on('click', function (e) {
+        // auguement quantité
+        $(".PDQPLUSNEW").on('click', function (e) {
+           
+             
+            var number = Number($(this).prev('.produit_commande_cible--qteNumber').text());
+            if(number<4 )
+            {
+                number++;
+                var prix = Number($(this).prev().prev().prev().prev('.produit_commande_cible--prixFixe').text());
+                $(this).prev().prev().prev('.produit_commande_cible--prix').text((prix*number).toFixed(2));
+                $(this).prev('.produit_commande_cible--qteNumber').text(number);
+                $(this).html("<button class='id_btn_plus'>+</button>");
+                $(this).prev().prev('.produit_commande_cible--qte-NEW').html("<button class='id_btn_plus'>-</button>");
+            }
+            else
+            {
+                  number--;
+                $(this).html("<button style='disabled:true;' class='id_btn_plus'><i style='color:red;' class='far fa-hand-paper'></i></button>");
+               
+                
 
-
-
-
-
-        var number = Number($(this).next('.produit_commande_cible--qteNumber').text());
-
-
-
-        if (number > 1) {
-            number--;
-            var prix = Number($(this).prev().prev('.produit_commande_cible--prixFixe').text());
-            var expression = prix * number;
-            $(this).prev('.produit_commande_cible--prix').text(expression);
-
-
-            $(this).next('.produit_commande_cible--qteNumber').text(number);
-            $(this).next('.produit_commande_cible--qteNumber').text(number);
-            $(this).html("<button class='id_btn_plus'>-</button>");
-            $(this).next().next('.PDQPLUS').html("<button class='id_btn_plus'>+</button>");
-
-        } else {
-
-            $(this).next('.produit_commande_cible--qteNumber').text("0");
-
-            $(this).html("<button  class='class_btn_minus'><i style='color:red;' class='fas fa-trash-alt'></i></button>");
-
-        }
-
-        calcul_price();
-        // alert($(this).attr('id'));
-        // console.log(number);
-
-    });
-
-    // auguement quantité
-    $(".PDQPLUS").on('click', function (e) {
-
-
-
-
-        var number = Number($(this).prev('.produit_commande_cible--qteNumber').text());
-
-
-        if (number < 4) {
-            number++;
-            var prix = Number($(this).prev().prev().prev().prev('.produit_commande_cible--prixFixe').text());
-
-            $(this).prev().prev().prev('.produit_commande_cible--prix').text(prix * number);
-
-
-            $(this).prev('.produit_commande_cible--qteNumber').text(number);
-            $(this).html("<button class='id_btn_plus'>+</button>");
-            $(this).prev().prev('.produit_commande_cible--qte-').html("<button class='id_btn_plus'>-</button>");
-
-        } else {
-
-            $(this).html("<button class='id_btn_plus'><i style='color:red;' class='far fa-hand-paper'></i></button>");
-
-        }
-
-        calcul_price();
-        // alert($(this).attr('id'));
-        // console.log(number);
-
-    });
-
-
+            }
+            remplir_panier();
+            calcul_price();
+           
+    
+        });
+        
+       
 
 
 
 });
-
-// for(var i=0;i<BTNplus.length;i++)
-// {
-//     BTNplus[i].addEventListener('click',()=>{
-//         var cpt = Number(QteSpanValue[i].innerText);
-//         if(cpt<4)
-//         {
-//             cpt++;
-//             QteSpanValue[i].innerText=cpt;
-//         }
-//         else
-//         {
-//             var btnValuePlus= document.getElementsByClassName('id_btn_plus');
-
-//             btnValuePlus[i].innerHTML="<i class='far fa-hand-paper'></i>";
-//             btnValuePlus[i].style.color="red";
-
-//         }
-
-
-//     })
-
-//     BTNminus[0].addEventListener('click',()=>{
-//         var cpt = Number(QteSpanValue.innerText);
-//         if(cpt>1)
-//         {
-//             cpt--;
-//         QteSpanValue.innerText=cpt;
-//         }
-//         else
-//         {
-//             BTNminus.innerHTML="<i class='fas fa-trash-alt'></i>";
-//         }
-
-
-//     })
-// }
-
-// function Panier(){
-//     var produit = document.getElementsByName('produit');
-//     var btn = document.getElementById("confirmer");
-//     var produitSelect =[];
-//     var j=0;
-//     var prix = document.getElementsByClassName('Prix');
-//     var quantite = document.getElementById('quantite');
-//     var Somme=0;
-//     for (var i=0;i<produit.length;i++)
-//     {
-//         if(produit[i].checked)
-//         {
-//             produitSelect[j]=produit[i].id;
-//             j++;
-//             Somme += Number(prix[i].innerHTML);
-//         }
-//     }
-//     // alert(produitSelect);
-//     // alert("Total prix = " +Somme);
-//     quantite.innerText = "Le prix total est : "+ Somme + "DHS";
-
-// }
 
 function calcul_price() {
     var totalPrix = document.getElementsByClassName('container_body_right--rightInfos_prix');
@@ -159,6 +82,7 @@ function calcul_price() {
     totalPrix[0].innerHTML = Number(sommePrix.toFixed(2));
     totalPrix[1].innerHTML = Number(sommePrix.toFixed(2));
     livraison_msg(sommePrix);
+    return 1;
 
 
 }
@@ -170,5 +94,6 @@ function livraison_msg(prix) {
     } else {
         div_toDisplay[0].style.visibility = "visible";
     }
+    return
 
 }
--- Public/html/script.php
+++ Public/html/script.php
@@ -2,18 +2,22 @@
     $(document).ready(function () {
 
     $(window).scroll(function () {
+        
         if ($(this).scrollTop() > 50) {
             $("#Header").css("background-color", "white");
-            $("#Panier").css("background-color", "black");
+            $("#id_form_panier").css("background-color", "black");
             $("#Paragpanier").css("color", "white");
             $("#imgpanier").attr("src","../img/panier2.png");
             $("#imgpanier").css("width","45px");
             $("#imgpanier").css("height","45px");
+            $("#BtnPanier").css("background-color", "black");
+            
 
 
         } else {
             
-            $("#Panier").css("background-color", "white");
+            $("#id_form_panier").css("background-color", "white");
+            $("#BtnPanier").css("background-color", "white");
             $("#Header").css("background-color", "tranparent");
             $("#Paragpanier").css("color", "black");
             $("#imgpanier").attr("src","../img/panier.png");
@@ -21,12 +25,15 @@
             
         }
     });
-
+ 
     $(function () {
+        
+       
     
     //diminue quantité
-
+ 
         $(".produit_commande_cible--qte-").on('click', function (e) {
+          
             var number = Number($(this).next('.produit_commande_cible--qteNumber').text());
             if(number>0)
             {
@@ -41,15 +48,20 @@
             }
             else
             {
-                //number++;
+                 number++;
                 $(this).html("<button  class='class_btn_minus'><i style='color:red;' class='fas fa-trash-alt'></i></button>");
                 $(this).next('.produit_commande_cible--qteNumber').text("0");
             }
+            remplir_panier();
+        
+     
     
         });
 
         // auguement quantité
         $(".PDQPLUS").on('click', function (e) {
+           
+             
             var number = Number($(this).prev('.produit_commande_cible--qteNumber').text());
             if(number<4 )
             {
@@ -62,11 +74,14 @@
             }
             else
             {
-                //number--;
-                $(this).html("<button class='id_btn_plus'><i style='color:red;' class='far fa-hand-paper'></i></button>");
+                number--;
+                $(this).html("<button style='disabled:true;' class='id_btn_plus'><i style='color:red;' class='far fa-hand-paper'></i></button>");
+               
                 
 
             }
+            remplir_panier();
+           
     
         });
         
@@ -76,6 +91,80 @@
 
 });
 })
+        var allProduit=document.getElementsByClassName('produit_commande_cible--IdProduit');
+        var Qte_Valide=document.getElementsByClassName('produit_commande_cible--qteNumber');
+        //pour stocké les données dans des inputs invisible pour les transmettres a php
+        var QTE_TOSEND =  document.getElementById('QTE_SENDER');
+        var ID_TOSEND =  document.getElementById('ID_SENDER');
+
+        var ALLprix=document.getElementsByClassName('produit_commande_cible--prix');
+
+        var panier = document.getElementById('Paragpanier');
+        var btn_clicked=document.getElementsByClassName('PDQPLUS');
+
+ 
+// Modification panier
+function remplir_panier()
+{
+ 
+
+    var id_Produits =[];
+
+    var Qte_produits=[];
+
+    var Prix_produits=[];
+
+    var j=0;
+
+    var somme=0;
+
+
+
+for(var i=0;i<allProduit.length;i++)
+{
+    if((Number(Qte_Valide[i].innerHTML)>0))
+    {
+        // recuperation id produit seletionné from div hidden
+        id_Produits[j]=allProduit[i].innerHTML;
+        // le choix si on click sur plus ou moins
+        
+            Qte_produits[j]=Number(Qte_Valide[i].innerHTML);
+
+            //Prix produit
+
+            Prix_produits[j]=Number(ALLprix[i].innerHTML);
+
+            somme+=Prix_produits[j];
+             
+        
+       
+        
+        j++;
+        
+ 
+    }
+
+}
+ 
+
+        // console.log(id_Produits);
+        // console.log(Qte_produits);
+        // console.log(Prix_produits);
+        // console.log(somme);
+        panier.innerHTML=somme.toFixed(2)+"$";
+        QTE_TOSEND.value=Qte_produits;
+        ID_TOSEND.value=id_Produits;
+        // console.log(QTE_TOSEND.value);
+        // console.log(ID_TOSEND.value);
+
+
+
+}
+
+
+
+
+
 </script>
 </body>
 </html>
